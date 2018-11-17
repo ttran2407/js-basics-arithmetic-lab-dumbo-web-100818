2066f6c0d4ac284f9a0e7b556ddb49f7143cba23
github username <github email address>
2018-11-16 19:45:18 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+const newID = oldID + 1000000000
+let ageIsValid = false
+if (Number.isInteger(currentAge)){ageIsValid = true}
+Number.isInteger()
+const randomNumber = Math.random() * 20
+const randomInteger = Math.floor(randomNumber)
+const randomUserID = 1000000000 + randomInteger
